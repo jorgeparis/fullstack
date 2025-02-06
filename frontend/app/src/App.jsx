03deaec40f5016e5e023b07df7871cbacaadf177
Jorge Paris <mrjorgeparis@gmail.com>
2025-02-06 23:43:14 +0200

    060225 Completed the login screen

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
       <div className="main-app">
-        <Login />
+        <Login/>
       </div>
     </>
   );
--- frontend/app/src/pages/Login.jsx
+++ frontend/app/src/pages/Login.jsx
@@ -5,10 +5,20 @@ import "../css/Login.css";
 export default function Login() {
   return (
     <>
-      <div className="login">
-        <Input type="text" placeholder={"Username"} />
-        <Input type="password" placeholder={"Password"} />
-        <Button type="submit" value="LOGIN" />
+      <div className="login-container">
+        <div className="container-right">
+          <div className="container-right-text">
+            <h1>Welcome</h1>
+            <Button type="submit" value="Sign Up" />
+          
+          </div>
+        </div>
+        <div className="login-field-container">
+          <h1>Login</h1>
+          <Input type="text" placeholder={"Username"} />
+          <Input type="password" placeholder={"Password"} />
+          <Button type="submit" value="LOGIN" />
+        </div>
       </div>
     </>
   );
